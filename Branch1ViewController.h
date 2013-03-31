945e32c9e080b7f891a25572ae51d1e43e98f825
piaoapiao <tianma.123456@163.com>
2013-03-31 11:09:45 +0800

    commit for branch test2

---
+++ Branch1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Branch1ViewController.h
+//  ForMySelf
+//
+//  Created by wgdadmin on 13-3-31.
+//  Copyright (c) 2013年 wgdadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Branch1ViewController : UIViewController
+
+@end
---
+++ Branch1ViewController.m
@@ -0,0 +1,38 @@
+//
+//  Branch1ViewController.m
+//  ForMySelf
+//
+//  Created by wgdadmin on 13-3-31.
+//  Copyright (c) 2013年 wgdadmin. All rights reserved.
+//
+
+#import "Branch1ViewController.h"
+
+@interface Branch1ViewController ()
+
+@end
+
+@implementation Branch1ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
